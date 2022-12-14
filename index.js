1273e25dd0b6d1913f0c19b51817417c17db4f0a
Kevin <kelvinngechu@gmail.com>
2022-12-14 11:30:13 +0300

    not comp

--- index.js
+++ index.js
@@ -22,3 +22,37 @@ let createTimeInEvent = function(employee, dateStamp){
     })
     return employee
 }
+let hoursWorkedOnDate = function(employee, searchDate){
+    let tInEvent = employee.timeInEvents.find(function(emps){
+      return emps.date == searchDate
+    })
+    let tOutEvent =employee.timeOutEvents.find(function(emps){
+        return emps.date == searchDate
+    })
+    return (tOutEvent.hour - tInEvent.hour) / 100
+
+}
+let wagesEarnedOnDate = function(employee, specDate){
+    let hrWage = hoursWorkedOnDate(fun(employee, specDate)*employee.payPerHour)
+    return parseFloat(hrWage.toString())
+}
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(emps){
+        return emps.date
+    })
+    let owed = eligibleDates.reduce(function(note, m)
+    {
+        return note + wagesEarnedOnDate(employee, m)
+    })
+    return owed
+}
+let findByTitle= function(srcArray, title){
+    return srcArray.find(function(fnde){
+      return fnde.title === title
+    })
+}
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(note, fnde){
+        return note + allWagesFor(fnde)
+    })
+}
