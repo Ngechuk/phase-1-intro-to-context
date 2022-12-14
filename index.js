2179a45754233188831394b58c42f77401042b66
Kevin <kelvinngechu@gmail.com>
2022-12-14 09:27:42 +0300

    Employee record

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+const createEmployeeRecord = function(){
+    return {firstName:[0],
+           familyName:[1],
+           title:[2],
+           payPerHour:[3],
+           timeInEvents:[],
+           timeOutEvents:[]
+
+    }}
